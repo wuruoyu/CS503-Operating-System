52f4626ca6bf055aae99bbea3b5dd710ab703bf5
Ruoyu Wu <wu1377@xinu01.cs.purdue.edu>
2019-09-05 16:48:43 -0400

    lab0.3

---
+++ xinu-cs503-fall2019/system/hello.c
@@ -0,0 +1,13 @@
+/* hello.c - hello */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  hello - HELLO WORLD! 
+ *------------------------------------------------------------------------
+ */
+syscall hello(void)
+{
+	kprintf("Hello system call invoked\n");
+	return 1;
+}
--- xinu-cs503-fall2019/system/main.c
+++ xinu-cs503-fall2019/system/main.c
@@ -9,7 +9,7 @@ process	main(void)
 
 	recvclr();
 	pid32 pid = create(shell, 8192, 50, "shell", 1, CONSOLE);
-	kprintf("\n\nSpawning new shell with PID = %d\n\n", pid);
+	kprintf("pawning new shell with PID = %d\n", pid);
 	resume(pid);
 
 	/* Wait for shell to exit and recreate it */
@@ -18,7 +18,7 @@ process	main(void)
 		receive();
 		sleepms(200);
 		pid32 pid = create(shell, 4096, 20, "shell", 1, CONSOLE);
-		kprintf("\n\nSpawning new shell with PID = %d\n\n", pid);
+		kprintf("Spawning new shell with PID = %d\n", pid);
 		resume(pid);
 	}
 	return OK;
