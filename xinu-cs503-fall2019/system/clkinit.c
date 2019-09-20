f13a8cb06cad769b384547b65c48a0e376ca2aba
Ruoyu <wuruoyu@me.com>
2019-09-20 08:14:05 -0400

    add a clock

--- xinu-cs503-fall2019/system/clkinit.c
+++ xinu-cs503-fall2019/system/clkinit.c
@@ -4,6 +4,7 @@
 
 uint32	clktime;		/* Seconds since boot			*/
 uint32	count1000;		/* Milliseconds since last clock tick   */
+uint32  last_resched_ms; /* Milliseconds since last resched */
 qid16	sleepq;			/* Queue of sleeping processes		*/
 uint32	preempt;		/* Preemption counter			*/
 
@@ -27,7 +28,9 @@ void	clkinit(void)
 	/* Initialize the time since boot to zero */
 
 	clktime = 0;
-        count1000 = 0;
+  count1000 = 0;
+  last_resched_ms = 0;
+
 	/* Set interrupt vector for the clock to invoke clkdisp */
 
 	ioapic_irq2vec(2, IRQBASE);
